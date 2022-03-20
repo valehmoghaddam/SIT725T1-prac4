951f35d07309b1bc3515c71120427f2442b9e695
valehmoghaddam <valeh.moghaddam@deakin.edu.au>
2022-03-21 09:57:34 +1100

    update

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express()
 var port = process.env.port || 3000;
 
 
-app.use(express.static(__dirname+'/public'))
+ app.use(express.static(__dirname+'/public'))
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); 
 
 
 

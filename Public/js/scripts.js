1529d2bdf62bea270dbed2f19933e664a4f7ec3b
valehmoghaddam <valeh.moghaddam@deakin.edu.au>
2022-03-20 14:10:31 +1100

    adding cards

---
+++ Public/js/scripts.js
@@ -0,0 +1,41 @@
+const cardList = [
+    {
+        title: "Kuala 2",
+        image: "images/kuala2.jpeg",
+        link: "About kuala 2",
+        desciption: "Demo desciption about kuala 2"
+    },
+    {
+        title: "Kuala 3",
+        image: "images/kuala3.jpeg",
+        link: "About Kuala 3",
+        desciption: "Demo desciption about kuala 3"
+    }
+]
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '</div><div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+    '<div class="card-reveal">'+
+        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+        '<p class="card-text">'+item.desciption+'</p>'+
+      '</div></div></div>';
+      $("#card-section").append(itemToAppend)
+    });
+}
+
+
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#clickMeButton').click(()=>{
+        clickMe();
+    })
+    addCards(cardList);
+  });
